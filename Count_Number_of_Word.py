ef8c28d537e8efb54b89b59541a7433f0aef182f
Xuebin Wei <weixuebin@gmail.com>
2017-03-15 21:49:23 -0400

    filtered stop words

--- Count_Number_of_Word.py
+++ Count_Number_of_Word.py
@@ -11,6 +11,8 @@ and write the result to an excel file
 '''
 import xlwt        
 from collections import Counter        
+from nltk.corpus import stopwords
+stop = set(stopwords.words('english'))
         
 book = xlwt.Workbook() # create a new excel file
 sheet_test = book.add_sheet('word_count') # add a new sheet
@@ -19,7 +21,11 @@ sheet_test.write(i,0,'word') # write the header of the first column
 sheet_test.write(i,1,'count') # write the header of the second column
 
 with open('C:\\Project\\JMU\\2017 Spring\\IA241\\week8\\jmu_news.txt','r') as jmu_news:
-    count_result =  Counter(jmu_news.read().split()) # use Counter to count the occurrence of each word
+    # convert all the word into lower cases
+    # filter out stop words
+    word_list = [i for i in jmu_news.read().lower().split() if i not in stop]
+    
+    count_result =  Counter(word_list) # use Counter to count the occurrence of each word
     for result in count_result.most_common(10): # find the 10 most common words
         i = i+1 
         sheet_test.write(i,0,result[0])
