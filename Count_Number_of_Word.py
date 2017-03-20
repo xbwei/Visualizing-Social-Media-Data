6dd51dd9a0858dca73fdcdea056e6df880941fb0
Xuebin Wei <weixuebin@gmail.com>
2017-03-20 09:23:37 -0400

    calculate ratio of each word

--- Count_Number_of_Word.py
+++ Count_Number_of_Word.py
@@ -13,22 +13,26 @@ import xlwt
 from collections import Counter        
 from nltk.corpus import stopwords
 stop = set(stopwords.words('english'))
-        
+  
 book = xlwt.Workbook() # create a new excel file
 sheet_test = book.add_sheet('word_count') # add a new sheet
 i = 0
 sheet_test.write(i,0,'word') # write the header of the first column
 sheet_test.write(i,1,'count') # write the header of the second column
-
-with open('C:\\Project\\JMU\\2017 Spring\\IA241\\week8\\jmu_news.txt','r') as jmu_news:
+sheet_test.write(i,2,'ratio') # write the header of the third column
+    
+with open('C:\\Project\\JMU\\2017 Spring\\IA241\\week11\\jmu_news.txt','r') as jmu_news:
+     
     # convert all the word into lower cases
     # filter out stop words
     word_list = [i for i in jmu_news.read().lower().split() if i not in stop]
-    
-    count_result =  Counter(word_list) # use Counter to count the occurrence of each word
-    for result in count_result.most_common(10): # find the 10 most common words
+    word_total = word_list.__len__()
+     
+    count_result =  Counter(word_list)
+    for result in count_result.most_common(10):
         i = i+1 
         sheet_test.write(i,0,result[0])
         sheet_test.write(i,1,result[1])
-
-book.save('C:\\Project\\JMU\\2017 Spring\\IA241\\week8\\word_count.xls')
+        sheet_test.write(i,2,(result[1]/word_total))
+    
+book.save('C:\\Project\\JMU\\2017 Spring\\IA241\\week11\\word_count.xls')
