468d8b6fbe1459a16245870f5d53c4daea94961b
Xuebin Wei <weixuebin@gmail.com>
2017-03-30 10:22:15 -0400

    Update Mongo_Twitter_to_Access.py

--- Visualize Twitter Data/Mongo_Twitter_to_Access.py
+++ Visualize Twitter Data/Mongo_Twitter_to_Access.py
@@ -21,7 +21,7 @@ tweet_collection.create_index([("coordinates", pymongo.ASCENDING)])
 tweet_cursor = tweet_collection.find({'coordinates' : {"$ne" : None}}) # get tweet with coordinates
 num_of_tweet = float(tweet_cursor.count())
 
-db_file = 'C:\\Project\\Paper\\Homeless\\tweet.mdb' #change the location to your Access file
+db_file = '' #change the location to your Access file
 
 odbc_conn_str = 'DRIVER={Microsoft Access Driver (*.mdb)};DBQ=%s' %(db_file) 
 
@@ -33,7 +33,7 @@ for document in tweet_cursor:
     try:
         tweet_created_at = document["created_at"].split()
      
-         
+
         tweet_month = strptime(tweet_created_at[1],'%b').tm_mon
         if tweet_month<10:
             tweet_month_str = '0'+ str(tweet_month)
@@ -50,7 +50,7 @@ for document in tweet_cursor:
         print(i/num_of_tweet)
         sql_insert_statement = """insert into tweet(tweet_id, user_id,tweet_date, tweet_time, tweet_date_time, x, y,z) values('{}','{}','{}','{}','{}','{}','{}','{}');""".format\
                                  (document['id_str'],document["user"]["id_str"],tweet_date,tweet_time,tweet_date_time,document["coordinates"]['coordinates'][0],document["coordinates"]['coordinates'][1],z)
-    #     print (sql_insert_statement)
+
         cursor.execute(sql_insert_statement)
         cursor.commit()
     except:
