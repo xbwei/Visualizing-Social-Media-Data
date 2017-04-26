29bae585dd2bcfb2ba7b75f2d2bd4c5d0b60cefd
Xuebin Wei <weixuebin@gmail.com>
2017-04-26 16:50:03 -0400

    Create Mongo_Twitter_to_Place_Access_Table.sql

---
+++ Visualize Twitter Data/Mongo_Twitter_to_Place_Access_Table.sql
@@ -0,0 +1,13 @@
+create table tweet (
+tweet_id  Text,
+user_id Text,
+tweet_date Text,
+tweet_time Text,
+tweet_date_time Text,
+x Double,
+y Double,
+z Double,
+place Text,
+
+PRIMARY KEY(tweet_id)
+);
