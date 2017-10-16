5e1293468c491216d110b11e51c78e124b2e7f5f
Xuebin Wei <weixuebin@gmail.com>
2017-10-16 10:26:08 -0400

    Update Mongo_Twitter_to_Access.py

--- Visualize Twitter Data/Mongo_Twitter_to_Access.py
+++ Visualize Twitter Data/Mongo_Twitter_to_Access.py
@@ -4,7 +4,6 @@ Created on Mar 29, 2017
 @author: Xuebin Wei
 www.lbsocial.net
 '''
-
 import pymongo
 from pymongo import MongoClient
 
@@ -12,15 +11,24 @@ from time import mktime
 from datetime import datetime
 from time import strptime
 import pyodbc 
-client = MongoClient()
+client = MongoClient()#type your own server configuration 
 
-db = client.tweet_db # change to your tweet db
+db = client.gpdemo # change to your tweet database
+print (db.collection_names())
 
-tweet_collection = db.tweet_collection # change to your tweet collection
+
+tweet_collection = db.tweet_collection_gis # change to your tweet collection
+tweet_cursor = tweet_collection.find()
+ 
+print (tweet_cursor.count())
+
 tweet_collection.create_index([("id", pymongo.ASCENDING)],unique = True)
 tweet_collection.create_index([("coordinates", pymongo.ASCENDING)])
-tweet_cursor = tweet_collection.find({'coordinates' : {"$ne" : None}}) # get tweet with coordinates
+
+tweet_cursor = tweet_collection.find({'coordinates' : {"$ne" : None}}) # get tweets with coordinates
+
 num_of_tweet = float(tweet_cursor.count())
+print (num_of_tweet)
 
 db_file = '' #change the location to your Access file
 
@@ -35,7 +43,7 @@ for document in tweet_cursor:
     try:
         tweet_created_at = document["created_at"].split()
      
-
+    
         tweet_month = strptime(tweet_created_at[1],'%b').tm_mon
         if tweet_month<10:
             tweet_month_str = '0'+ str(tweet_month)
@@ -46,13 +54,13 @@ for document in tweet_cursor:
         tweet_time = tweet_created_at[3]
         tweet_date_time = tweet_date +' '+tweet_time
         time_diff = datetime.now()-datetime.fromtimestamp(mktime(strptime(tweet_date_time, "%Y-%m-%d %H:%M:%S"))) 
-        z =  time_diff.total_seconds()/5000000.0 # height of tweet points, relative time difference to current time, recent tweets on bottom
+        z =  (time_diff.total_seconds()+4.0*60*60)/50000.0 # height of tweet points, relative time difference to current time, recent tweets on top
          
         i = i+1 
         print(i/num_of_tweet)
         sql_insert_statement = """insert into tweet(tweet_id, user_id,tweet_date, tweet_time, tweet_date_time, x, y,z, retweet, favorite) values('{}','{}','{}','{}','{}',{},{},{},{},{});""".format\
                                  (document['id_str'],document["user"]["id_str"],tweet_date,tweet_time,tweet_date_time,document["coordinates"]['coordinates'][0],document["coordinates"]['coordinates'][1],z,document['retweet_count'],document['favorite_count'])
-
+    
         cursor.execute(sql_insert_statement)
         cursor.commit()
     except:
