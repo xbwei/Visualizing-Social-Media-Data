f1b4f7b13c5dca514828a708fba17f069fe08e45
Xuebin Wei <weixuebin@gmail.com>
2019-03-19 10:33:59 -0400

    Update Twitter2gephi.py

--- Visualize Twitter Data/Twitter2gephi.py
+++ Visualize Twitter Data/Twitter2gephi.py
@@ -20,22 +20,22 @@ for row in reader:
 
     people = []
     people.append(row['tweetuser'])
-    if row['mention']<>'':
+    if row['mention']!='':
 #         print row['mention']
         i = i+1
         for mentioned_user in (row['mention'].split(",")[:-1]):
             
 #             print '\t',mentioned_user
             people.append(mentioned_user)
-    if row['replyuser'] <>'':
+    if row['replyuser'] !='':
         people.append(row['replyuser'])
-    if row['retweetuser'] <>'':
+    if row['retweetuser'] !='':
         people.append(row['retweetuser'])
     unique_people = list(set(people))
     if len(unique_people) > 1:
         for people1 in unique_people:
             for people2 in unique_people:
-                if people1 <> people2:
+                if people1 != people2:
                     if nxg.has_edge(people1, people2):
                         nxg[people1][people2]['weight'] =0.5 + nxg[people1][people2]['weight']
         #                             print nxg.number_of_edges(people1, people2)
